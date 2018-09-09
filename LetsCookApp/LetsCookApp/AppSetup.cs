40a6086228c146947019f86df19d4778b0f1b497
santosh <santosh@MYPC>
2018-09-09 23:02:11 +0530

    First Commit

---
+++ LetsCookApp/LetsCookApp/AppSetup.cs
@@ -0,0 +1,34 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using LetsCookApp.Managers.ApiProvider;
+using LetsCookApp.Managers.SettingsManager;
+using LetsCookApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsCookApp
+{
+    public class AppSetup
+    {
+        public AppSetup()
+        {
+            //Viewmodel
+            SimpleIoc.Default.Register<LoginViewModel>();
+
+
+            // Services
+            SimpleIoc.Default.Register<ISettingsManager, SettingsManager>();
+            SimpleIoc.Default.Register<IApiProvider, ApiProvider>();
+            SimpleIoc.Default.Register<IManager, Manager>();
+        }
+        public LoginViewModel LoginViewModel
+        {
+            get
+            {
+                return SimpleIoc.Default.GetInstance<LoginViewModel>();
+            }
+        }
+    }
+}
---
+++ LetsCookApp/LetsCookApp/Views/SignInSignUpView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace LetsCookApp.Views
+{
+    public partial class SignInSignUpView : ContentPage
+    {
+        public SignInSignUpView()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+    }
+}
